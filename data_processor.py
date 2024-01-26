307d110a31d6c4b86e1b18eddd4b3a6c992e123e
davidhy8 <david.yang1@ucalgary.ca>
2024-01-25 17:54:38 -0700

    fixed merging between dataframe errors

--- data_processor.py
+++ data_processor.py
@@ -2,7 +2,7 @@ import pandas
 import datetime
 from datetime import date
 import numpy as np
-import matplotlib.pyplot as mp
+# import matplotlib.pyplot as mp
 
 
 # Load and process raw COVID-19 data and returns statistics for each month
@@ -45,6 +45,16 @@ def model_data():
     df_ihme.columns = ['location', 'date', 'IHME_estimate_mean']
     df_lshtm.columns = ['location', 'date', 'LSHTM_estimate_median']
     df_yyg.columns = ['location', 'date', 'YYG_estimate_mean']
+
+    df_icl['date'] = pandas.to_datetime(df_icl['date'])
+    df_ihme['date'] = pandas.to_datetime(df_ihme['date'])
+    df_lshtm['date'] = pandas.to_datetime(df_lshtm['date'])
+    df_yyg['date'] = pandas.to_datetime(df_yyg['date'])
+
+    df_icl['date'] = df_icl['date'].dt.to_period('M')
+    df_ihme['date'] = df_ihme['date'].dt.to_period('M')
+    df_lshtm['date'] = df_lshtm['date'].dt.to_period('M')
+    df_yyg['date'] = df_yyg['date'].dt.to_period('M')
 
     df_icl = df_icl.groupby(['location', 'date'], as_index=False).sum()
     df_ihme = df_ihme.groupby(['location', 'date']).sum().reset_index()
@@ -95,29 +105,29 @@ def sero_data():
     df_sero = df_sero[df_sero['seroprevalence'].notna()]
     df_sero = df_sero[df_sero['participants'].notna()]
 
-    df_sero = df_sero.groupby(['location', 'date'], as_index=False).agg(sero_study_participants=('participants', np.sum),
-                                                                        avg_seroprevalence=('seroprevalence',
-                                                                                  lambda x: weighted_mean(x,
-                                                                                                          df_sero.loc[
-                                                                                                              x.index, 'participants'])))
-    df_sero.to_csv('sero.csv', header=True, index=False)
+    df_sero = df_sero.groupby(['location', 'date']).agg(sero_study_participants=('participants', np.sum), avg_seroprevalence=('seroprevalence', lambda x: weighted_mean(x, df_sero.loc[x.index, 'participants']))).reset_index()
+    # print(df_sero.head())
+
+    df_sero.to_csv('output/sero.csv', header=True, index=False)
 
     return df_sero
 
 
 # Load SARS-CoV-2 sequence metadata from https://www.gisaid.org
 def sequence_counter():
-    df_sequences = pandas.read_table('data/metadata.tsv', dtype={'country': str, 'date': str}, usecols={'country', 'date'}, sep='\t')
-
-    df_sequences['date'] = df_sequences['date'].replace({"XX": "01"}, regex=True)
+    df_sequences = pandas.read_table('data/metadata.tsv', dtype={'country': str, 'date': str}, usecols={'Location', 'Collection date'}, sep='\t')
     df_sequences.columns = ['date', 'location']
+    df_sequences['location'] = df_sequences['location'].str.split("/ ").str[1]
+    df_sequences['location'] = df_sequences['location'].str.strip()
+    print(df_sequences.head())
+    df_sequences['date'] = df_sequences['date'].replace({"XX": "01"}, regex=True)
 
     df_sequences['date'] = pandas.to_datetime(df_sequences['date'], errors = 'coerce')
     df_sequences = df_sequences.dropna(subset=['date'])
     df_sequences['date'] = df_sequences['date'].dt.to_period('M')
-    df_sequences = df_sequences.groupby(['date', 'location']).size().to_frame('sequences')
+    df_sequences = df_sequences.groupby(['date', 'location']).size().to_frame('sequences').reset_index()
 
-    df_sequences.to_csv('output/metadata_grouped.tsv', sep='\t', header=True)
+    df_sequences.to_csv('output/metadata_grouped.csv', header=True, index=False)
     return df_sequences
 
 
@@ -135,7 +145,11 @@ def merge_df(df_raw, df_models, df_sero, df_sequences):
 
 # Filter the dataframes by start and end date in the format YYYY-MM-DD and calculate various statistics
 def calc_df(start_date, end_date, df, num_of_seq):
-    df = df[df['date'] >= start_date] & df[df['date'] <= end_date]
+    start_date = pandas.to_datetime(start_date)
+    end_date = pandas.to_datetime(end_date)
+
+    df['date'] = pandas.to_datetime(df['date'])
+    df = df[df['date'].between(start_date, end_date)]
     months = pandas.date_range(start_date, end_date, freq='MS').strftime("%Y-%m").tolist()
     sequences = {}
     estimates = {}
@@ -157,11 +171,11 @@ def calc_df(start_date, end_date, df, num_of_seq):
         sequences_ideal = estimates[month]/estimate_all * num_of_seq
 
         df.loc[df['date'] == month, 'estimate_contribution'] = df['IHME_estimate_mean'] / estimates[month]
-        df.loc[df['date'] == month, 'ideal_sequences'] = df['estimate_contribution'] * sequences_ideal[month]
+        df.loc[df['date'] == month, 'ideal_sequences'] = df['estimate_contribution'] * sequences_ideal
 
         cases[month] = df.loc[(df['location'] == 'World') & (df['date'] == month), 'cases'].values[0]
         df.loc[df['date'] == month, 'case_contribution'] = df['cases'] / cases[month]
-        df.loc[df['date'] == month, 'apparent_sequences'] = df['case_contribution'] * sequences_ideal[month]
+        df.loc[df['date'] == month, 'apparent_sequences'] = df['case_contribution'] * sequences_ideal
 
     df_estimate_outlier = df.loc[df['sequences'] < df['ideal_sequences']]
     df_case_outlier = df.loc[df['sequences'] < df['apparent_sequences']]
@@ -180,12 +194,27 @@ def calc_df(start_date, end_date, df, num_of_seq):
     return df
 
 if __name__ == '__main__':
-    df_raw = raw_data()
-    df_models = model_data()
-    df_sero = sero_data()
-    df_sequences = sequence_counter()
+    # df_raw = raw_data()
+    df_raw = pandas.read_csv('output/ourworldindata_grouped.csv')
+    print("raw data loaded")
+
+    # df_models = model_data()
+    df_models = pandas.read_csv('output/country_estimates.csv')
+    print("model data loaded")
+
+    # df_sero = sero_data()
+    df_sero = pandas.read_csv('output/sero.csv')
+    print("serology data loaded")
+
+    # df_sequences = sequence_counter()
+    df_sequences = pandas.read_csv("output/metadata_grouped.csv")
+    print("sequence data loaded")
+
     df_merged = merge_df(df_raw, df_models, df_sero, df_sequences)
+    print("dataframes merged")
+
     df = calc_df('2020-02', '2020-10', df_merged, 25000)
+    print("data processed")
 
 
 
